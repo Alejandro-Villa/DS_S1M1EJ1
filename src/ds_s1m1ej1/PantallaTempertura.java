e108580e0cb8cde0ffa823778af873a53a0ab601
Alejandro-Villa <awewanwo@disroot.org>
2022-03-03 10:29:04 +0100

    Metodos basicos PantallaTemperatura

--- src/ds_s1m1ej1/PantallaTempertura.java
+++ src/ds_s1m1ej1/PantallaTempertura.java
@@ -9,6 +9,22 @@ package ds_s1m1ej1;
  *
  * @author antonio
  */
-public class PantallaTempertura {
+public class PantallaTemperatura { // Habia errata aqui
+    private TermometroObservable t;
+    float temp_actual;
     
+    // Constructor, se copia referencia al observable y se inica temp
+    public PantallaTemperatura(TermometroObservable termometro){
+        t = termometro;
+        temp_actual = t.getTemperatura();
+    }
+    
+    // Se pide temp al observable y si cambia se actualiza
+    float getNuevaTemperatura() {
+        float temp_nueva = t.getTemperatura();
+        if(temp_nueva != temp_actual)
+            temp_actual = temp_nueva;
+        
+        return temp_actual;
+    }
 }
